3d88f00ed77e33da3658459795fb5f1f9cf00b8c
Niaz Faridani-Rad <tigrexspalterlp@gmail.com>
2022-10-05 13:02:56 +0200

    Delete private-repo.sh

--- private-repo.sh
+++
@@ -1,3 +0,0 @@
-npm config set @atmokyaudio:registry https://gitlab.com/api/v4/projects/36729375/packages/npm/
-npm config set -- "//gitlab.com/api/v4/projects/36729375/packages/npm/:_authToken" ${ATMOKY_KEY}
-
