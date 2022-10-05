3ca337040b17b7a61125f29e799d439bd509bdb9
Niaz Faridani-Rad <tigrexspalterlp@gmail.com>
2022-10-05 12:56:05 +0200

    Update private-repo.sh

--- private-repo.sh
+++ private-repo.sh
@@ -1,7 +1,3 @@
-if [[ -z "${NETLIFY}" ]]; then
-  echo "Not netlify"
-else
-  echo "Netlify"
-  npm config set @atmokyaudio:registry https://gitlab.com/api/v4/projects/36729375/packages/npm/
-  npm config set -- "//gitlab.com/api/v4/projects/36729375/packages/npm/:_authToken" ${ATMOKY_KEY}
-fi
+npm config set @atmokyaudio:registry https://gitlab.com/api/v4/projects/36729375/packages/npm/
+npm config set -- "//gitlab.com/api/v4/projects/36729375/packages/npm/:_authToken" ${ATMOKY_KEY}
+
